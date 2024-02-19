eda0029f89608dff63b16f693ce4b7512147c4ed
mufti ramjan <alibinalaluddin@gmail.com>
2024-02-19 15:25:07 +0600

    add all

--- for.js
+++ for.js
@@ -10,7 +10,13 @@ const allbtn=document.getElementsByClassName('al-btn');
 let count=0;
 for(const btn of allbtn){
     btn.addEventListener('click',function(e){
+        btn.style.backgroundColor="green";
        count=count+1;
+    //    allbtn.classList.add("disabled",true)
+        if(count>=5){
+        return btn
+        }
+      
 
         const bdt=document.getElementById('bdt').innerText;
         const bdtTotal= parseInt(bdt);
@@ -18,8 +24,8 @@ for(const btn of allbtn){
         
     
         const seat=document.getElementById("seat");
-        const convertBtd=parseInt(seat);
-        document.getElementById("seat").innerText=convertBtd-40;
+        const convertBtd=parseInt(seat) ;
+        document.getElementById("seat").innerText=convertBtd-parseInt(40);
 
 
 
@@ -31,3 +37,9 @@ for(const btn of allbtn){
 function setinnerText(id,value){
     document.getElementById(id).innerText = value;
 }
+
+document.getElementById('nextBtn').addEventListener('click',function(){
+    var pop =document.getElementById("popUp");
+      pop.style.display="block";
+   
+})
